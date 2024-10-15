d89749b9e4043d6616d9510b6df66e5960c5ec6d
jaiba147 <felipehiba01@gmial.com>
2024-10-15 20:26:44 -0300

    started LSB1

--- steg/src/main/java/ar/edu/itba/Algorithm.java
+++ steg/src/main/java/ar/edu/itba/Algorithm.java
@@ -1,6 +1,8 @@
 package ar.edu.itba;
 
+import java.io.FileInputStream;
+
 public interface Algorithm {
 
-    void run();
+    void run(int blue, int green, int red, FileInputStream message, int messageBitCounter);
 }
--- steg/src/main/java/ar/edu/itba/algs/LSB1.java
+++ steg/src/main/java/ar/edu/itba/algs/LSB1.java
@@ -1,12 +1,14 @@
 package ar.edu.itba.algs;
 
+import java.io.FileInputStream;
+
 import ar.edu.itba.Algorithm;
 
 public class LSB1 implements Algorithm {
 
     @Override
-    public void run() {
-        System.out.println("running LSB1");
+    public void run(int blue, int green, int red, FileInputStream message, int messageBitCounter) {
+
     }
 
 }

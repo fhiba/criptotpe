42eb927e41de4993dc517031987e0832a0575bc9
jaiba147 <felipehiba01@gmial.com>
2024-10-15 19:48:06 -0300

    started hide function for embed

--- steg/src/main/java/ar/edu/itba/Embed.java
+++ steg/src/main/java/ar/edu/itba/Embed.java
@@ -1,8 +1,10 @@
 package ar.edu.itba;
 
-import java.util.function.Supplier;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 
-import ar.edu.itba.algs.*;
+import javax.imageio.ImageIO;
 
 public class Embed {
     private String filePath;
@@ -13,7 +15,7 @@ public class Embed {
     private String pass;
     private String enc;
 
-    public void embed(String[] args) {
+    public void embed(String[] args) throws Exception {
         filePath = args[2];
         bitmapFile = args[4];
         outFile = args[6];
@@ -22,8 +24,48 @@ public class Embed {
         mode = args[12];
         if (args[14] != null)
             pass = args[14];
-        //enc.setMode(mode);
-
+        // enc.setMode(mode);
+        hide();
     }
 
+    public void hide() throws Exception {
+        File bmpFile = new File(bitmapFile);
+        FileInputStream fis = new FileInputStream(bmpFile);
+        FileInputStream message = new FileInputStream(new File(filePath));
+
+        byte[] header = new byte[54]; // salteamos el header de bitmap v3
+        fis.read(header);
+
+        byte[] pixelData = new byte[fis.available()];
+        fis.read(pixelData);
+        fis.close();
+
+        BufferedImage image = ImageIO.read(bmpFile);
+
+        int width = image.getWidth();
+        int height = image.getWidth();
+
+        int messageBitCounter = 0;
+        int pixelIndex = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int blue = pixelData[pixelIndex] & 0xFF; // Blue
+                int green = pixelData[pixelIndex + 1] & 0xFF; // Green
+                int red = pixelData[pixelIndex + 2] & 0xFF; // Red
+
+                int rgb = (red << 16) | (green << 8) | blue;
+
+                // LSB(blue,green,red,message,messageBitCounter);
+                // UTILIZAR LSB aca para modificar los pixeles
+                image.setRGB(x, height - y - 1, rgb);
+
+                pixelIndex += 3;
+                messageBitCounter += 3;
+            }
+        }
+
+        // Save the image as a PNG (or any other format)
+        ImageIO.write(image, "png", new File("output_image.png"));
+        System.out.println("BMP converted to PNG and saved.");
+    }
 }

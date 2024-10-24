26194bfd5198db708d5b256bd81bbbae0563380f
Jose-Burgos <jburgos@itba.edu.ar>
2024-10-24 01:01:29 -0300

    fix embed method

--- steg/src/main/java/ar/edu/itba/Embed.java
+++ steg/src/main/java/ar/edu/itba/Embed.java
@@ -38,16 +38,22 @@ public class Embed {
   public void hide() throws Exception {
     File bmpFile = new File(bitmapFile);
     File msgFile = new File(filePath);
+
+    if (!msgFile.exists() || !msgFile.isFile()) {
+      throw new RuntimeException("Message file not found or invalid file path.");
+    }
+
     FileInputStream fis = new FileInputStream(bmpFile);
     FileInputStream message = new FileInputStream(msgFile);
     byte[] messageBytes = message.readAllBytes();
+    message.close();
+
     System.out.println("File path: " + filePath);
 
     String extension = null;
     long fullSize = 0;
 
     String[] auxStrings = filePath.split("\\.");
-
     if (auxStrings.length > 1) {
       extension = auxStrings[auxStrings.length - 1];
       System.out.println("Extracted extension: " + extension);
@@ -73,116 +79,64 @@ public class Embed {
       throw new RuntimeException("Cannot store the message in the image.");
     }
 
-    byte[] header = new byte[54]; // salteamos el header de bitmap v3
+    byte[] header = new byte[54];
     fis.read(header);
 
-    byte[] pixelData = new byte[fis.available()];
+    BufferedImage image = ImageIO.read(bmpFile);
+    int width = image.getWidth();
+    int height = image.getHeight();
+    int rowSize = (width * 3 + 3) & ~3;
+
+    byte[] pixelData = new byte[rowSize * height];
     fis.read(pixelData);
     fis.close();
 
-    BufferedImage image = ImageIO.read(bmpFile);
-
-    int width = image.getWidth();
-    int height = image.getHeight();
     System.out.println("Image width: " + width);
     System.out.println("Image height: " + height);
 
-    int messageBitCounter = 0;
-    int messageByteCounter = 0;
-    int pixelIndex = 0;
-    byte[] modifiedPixels;
-    byte blue = (byte) (pixelData[pixelIndex] & 0xFF); // Blue
-    byte green = (byte) (pixelData[pixelIndex + 1] & 0xFF); // Green
-    byte red = (byte) (pixelData[pixelIndex + 2] & 0xFF); // Red
-
-    if (pass != null) {
-      if (fullSize % 8 != 0)
-        fullSize += fullSize % 8;
-    }
-
     byte[] storeSize = ByteBuffer.allocate(Long.BYTES).putLong(fullSize).array();
     int sizeBitCounter = 0;
-    int sizeByteCounter = 3;
-    int x = 0, y = 0;
-    // steganografia del tamaño + padding y extension
-    for (y = 0; y < height; y++) {
-      for (x = 0; x < 4; x++) {
-        blue = (byte) (pixelData[pixelIndex] & 0xFF); // Blue
-        green = (byte) (pixelData[pixelIndex + 1] & 0xFF); // Green
-        red = (byte) (pixelData[pixelIndex + 2] & 0xFF); // Red
+    int sizeByteCounter = 0;
 
-        modifiedPixels = alg.run(blue, green, red, storeSize, sizeByteCounter, sizeBitCounter);
+    int messageBitCounter = 0;
+    int messageByteCounter = 0;
 
-        // me fijo si puedo sumar 3 en el bit counter o si tengo que adelantar el byte
-        if (sizeBitCounter + 3 * alg.getBitsUsed() >= 8) {
-          sizeByteCounter++;
-          sizeBitCounter = (sizeBitCounter + 3 * alg.getBitsUsed()) % 8;
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        int pixelIndex = y * rowSize + x * 3;
+
+        byte blue = (byte) (pixelData[pixelIndex] & 0xFF);
+        byte green = (byte) (pixelData[pixelIndex + 1] & 0xFF);
+        byte red = (byte) (pixelData[pixelIndex + 2] & 0xFF);
+
+        if (sizeByteCounter < storeSize.length) {
+          blue = (byte) ((blue & ~1) | ((storeSize[sizeByteCounter] >> sizeBitCounter) & 1));
+          sizeBitCounter++;
+          if (sizeBitCounter == 8) {
+            sizeBitCounter = 0;
+            sizeByteCounter++;
+          }
+        } else if (messageByteCounter < messageBytes.length) {
+          blue = (byte) ((blue & ~1) | ((messageBytes[messageByteCounter] >> messageBitCounter) & 1));
+          messageBitCounter++;
+          if (messageBitCounter == 8) {
+            messageBitCounter = 0;
+            messageByteCounter++;
+          }
         } else {
-          sizeBitCounter += 3 * alg.getBitsUsed();
+          break;
         }
 
-        int rgb = (modifiedPixels[0] << 16) | (modifiedPixels[1] << 8) | modifiedPixels[2];
+        int rgb = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
         image.setRGB(x, height - y - 1, rgb);
-
-        pixelIndex += 3;
       }
-    }
-
-    // byte[] encryptedMsg = enc.encrypt(messageBytes);
-    byte[] encryptedMsg = messageBytes;
-
-    // steganografia del mensaje
-    for (; y < height; y++) {
-      for (; x < width; x++) {
-        blue = (byte) (pixelData[pixelIndex] & 0xFF); // Blue
-        green = (byte) (pixelData[pixelIndex + 1] & 0xFF); // Green
-        red = (byte) (pixelData[pixelIndex + 2] & 0xFF); // Red
-
-        modifiedPixels = alg.run(blue, green, red, encryptedMsg, messageByteCounter, messageBitCounter);
-
-        // me fijo si puedo sumar 3 en el bit counter o si tengo que adelantar el byte
-        if (messageBitCounter + 3 * alg.getBitsUsed() >= 8) {
-          messageByteCounter++;
-          messageBitCounter = (messageBitCounter + 3 * alg.getBitsUsed()) % 8;
-        } else {
-          messageBitCounter += 3 * alg.getBitsUsed();
-        }
-
-        int rgb = (modifiedPixels[0] << 16) | (modifiedPixels[1] << 8) | modifiedPixels[2];
-        image.setRGB(x, height - y - 1, rgb);
-
-        pixelIndex += 3;
-      }
-    }
-
-    byte[] extensionBytes = extension.getBytes();
-    int exBitCounter = 0;
-    int exByteCounter = 0;
-
-    // steganografia de la extension
-    for (; y < height; y++) {
-      for (; x < width; x++) {
-        blue = (byte) (pixelData[pixelIndex] & 0xFF); // Blue
-        green = (byte) (pixelData[pixelIndex + 1] & 0xFF); // Green
-        red = (byte) (pixelData[pixelIndex + 2] & 0xFF); // Red
-
-        modifiedPixels = alg.run(blue, green, red, extensionBytes, exByteCounter, exBitCounter);
-
-        // me fijo si puedo sumar 3 en el bit counter o si tengo que adelantar el byte
-        if (exBitCounter + 3 * alg.getBitsUsed() >= 8) {
-          exByteCounter++;
-          exBitCounter = (exBitCounter + 3 * alg.getBitsUsed()) % 8;
-        } else {
-          exBitCounter += 3 * alg.getBitsUsed();
-        }
-
-        int rgb = (modifiedPixels[0] << 16) | (modifiedPixels[1] << 8) | modifiedPixels[2];
-        image.setRGB(x, height - y - 1, rgb);
-
-        pixelIndex += 3;
+      if (messageByteCounter >= messageBytes.length && sizeByteCounter >= storeSize.length) {
+        break;
       }
     }
 
     ImageIO.write(image, "bmp", new File(outFile));
+    System.out.println("Message hidden successfully.");
   }
+
 }
--- steg/src/main/java/ar/edu/itba/algs/LSB1.java
+++ steg/src/main/java/ar/edu/itba/algs/LSB1.java
@@ -1,47 +1,45 @@
 package ar.edu.itba.algs;
 
-import java.nio.ByteBuffer;
-
 import ar.edu.itba.Algorithm;
 
 public class LSB1 implements Algorithm {
 
-    Integer bitsUsed = 1;
+  Integer bitsUsed = 1;
 
-    @Override
-    public byte[] run(byte blue, byte green, byte red, byte[] message, int messageByteCounter, int messageBitCounter) {
-        int currentBit;
-        byte[] bytes = new byte[3];
-        bytes[0] = blue;
-        bytes[1] = red;
-        bytes[2] = green;
-        for (int i = 0; i < 3; i++) {
-            currentBit = (message[messageBitCounter] >> (7 - i)) & 1;
-            if (currentBit == 1) {
-                bytes[i] = (byte) (bytes[i] | 0b00000001); // Set the least significant bit to 1
-            } else {
-                bytes[i] = (byte) (bytes[i] & 0b11111110); // Set the least significant bit to 0
-            }
-            messageBitCounter++;
-            if (messageBitCounter == 8) {
-                messageByteCounter++;
-                messageBitCounter = 0;
-            }
-        }
-        return bytes;
+  @Override
+  public byte[] run(byte blue, byte green, byte red, byte[] message, int messageByteCounter, int messageBitCounter) {
+    int currentBit;
+    byte[] bytes = new byte[3];
+    bytes[0] = blue;
+    bytes[1] = red;
+    bytes[2] = green;
+    for (int i = 0; i < 3; i++) {
+      currentBit = (message[messageBitCounter] >> (7 - i)) & 1;
+      if (currentBit == 1) {
+        bytes[i] = (byte) (bytes[i] | 0b00000001); // Set the least significant bit to 1
+      } else {
+        bytes[i] = (byte) (bytes[i] & 0b11111110); // Set the least significant bit to 0
+      }
+      messageBitCounter++;
+      if (messageBitCounter == 8) {
+        messageByteCounter++;
+        messageBitCounter = 0;
+      }
     }
+    return bytes;
+  }
 
-    @Override
-    public void extract(byte forExtraction, byte[] msg, int byteCounter, int bitCounter) {
-        int currentBit;
+  @Override
+  public void extract(byte forExtraction, byte[] msg, int byteCounter, int bitCounter) {
+    int currentBit;
 
-        currentBit = forExtraction & 1;
+    currentBit = forExtraction & 1;
 
-        msg[byteCounter] = (byte) (msg[byteCounter] | (currentBit << (7 - bitCounter)));
-    }
+    msg[byteCounter] = (byte) (msg[byteCounter] | (currentBit << (7 - bitCounter)));
+  }
 
-    @Override
-    public Integer getBitsUsed() {
-        return bitsUsed;
-    }
+  @Override
+  public Integer getBitsUsed() {
+    return bitsUsed;
+  }
 }
